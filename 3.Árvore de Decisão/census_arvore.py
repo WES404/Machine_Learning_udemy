34109fa1fe42e7b0ef86f862a71eb01257c26de8
Weslei Rodrigues <69604469+crazywes@users.noreply.github.com>
2021-02-11 19:36:53 -0300

    Add files via upload

---
+++ 3.Árvore de Decisão/census_arvore.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+base = pd.read_csv("census.csv")
+
+previsores = base.iloc[:, 0:14].values
+
+classe = base.iloc[:, 14].values
+
+# Mudando dados categóricos para Númericos
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+labelenconder_previsores = LabelEncoder()
+
+onehotencoder = ColumnTransformer(transformers=[("OneHot", OneHotEncoder(), [1,3,5,6,7,8,9,13])],remainder='passthrough')
+
+# Muda os previsores para númericos sem 'peso'
+previsores = onehotencoder.fit_transform(previsores).toarray()
+
+# Muda as classes para numericas
+labelenconder_classes = LabelEncoder()
+classe = labelenconder_classes.fit_transform(classe)
+
+from sklearn.model_selection import train_test_split
+# Dividindo
+X_treino, X_teste, y_treino, y_teste = train_test_split(previsores, classe, test_size=.33, random_state=0)
+
+# Usando Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+classificador = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classificador.fit(X_treino, y_treino)
+
+# Pevendo Decision Tree
+previssoes = classificador.predict(X_teste)
+
+# Checando presição
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(y_teste, previssoes)
+
+matriz = confusion_matrix(y_teste, previssoes)
+
+""" Há maior precisao sem o escalonamento """
---
+++ 3.Árvore de Decisão/credit_data_arvore.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+base = pd.read_csv('credit_data.csv')
+
+# preencher valores com a média
+base.loc[base.age < 0, 'age'] = 40.92
+
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
+
+from sklearn.impute import SimpleImputer
+# Muda os valores NaN pela média
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean') 
+imputer = imputer.fit(previsores[:, 1:4])
+previsores[:, 1:4] = imputer.transform(previsores[:, 1:4])
+
+from sklearn.preprocessing import StandardScaler
+# Escalona pelo método padrão
+Escalar = StandardScaler()
+previsores = Escalar.fit_transform(previsores)
+
+# Separa o treinamento e o teste
+from sklearn.model_selection import train_test_split
+
+X_treino, X_teste, y_treino, y_teste = train_test_split(previsores, classe, test_size=.25, random_state=0)
+
+# Usando Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+classificador = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classificador.fit(X_treino, y_treino)
+
+# Pevendo Decision Tree
+previssoes = classificador.predict(X_teste)
+
+# Checando presição
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(y_teste, previssoes)
+matriz = confusion_matrix(y_teste, previssoes)
